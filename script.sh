d114736d8e67610ff6d5a4a5e95bb6b4392fbb0c
krish8688 <101493241+krish8688@users.noreply.github.com>
2022-03-13 22:58:24 +0530

    Update script.sh
    add and sub

--- script.sh
+++ script.sh
@@ -1,6 +1,20 @@
 echo "Hello World"
 
-#ping www.google.com
+
+echo " Please enter your choice ADD or SUB: $1"
+
+if ["$s1" == "ADD"]
+then
+  echo "You are in add loop "
+  Total =3+2
+  echo ":$Total"
+  
+fi
+
+echo " you are in sub loop"
+
+Total = 3-2
+echo ":$Total"
 
 ./nestedscript.sh  Saikrishna
 
