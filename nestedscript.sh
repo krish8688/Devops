4ffc936fbcd7291d58c3b84caeb7891c335d570b
krish8688 <101493241+krish8688@users.noreply.github.com>
2022-03-13 22:50:06 +0530

    Update nestedscript.sh
    script update

--- nestedscript.sh
+++ nestedscript.sh
@@ -1,8 +1,8 @@
 
 name=$1
 echo "calling Nested script inside script.sh"
-echo " here am printing out put nested.sh file"
+echo " here am printing output from nested.sh file"
 
-echo "Greate work:$1"
+echo "Great work:$1"
 exit
 
